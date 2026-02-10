82ada3a196bd68cd5457fe310a3ff6edb309cc36
Ash Kyd <kyd.ashley@abc.net.au>
2026-02-10 12:49:51 +1000

    initial commit

---
+++ src/components/App/App.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import Scrollyteller from '@abcnews/svelte-scrollyteller';
+  import type { PanelDefinition } from '@abcnews/svelte-scrollyteller';
+  import type { PanelData } from '../../index';
+  import { untrack } from 'svelte';
+  import { fade } from 'svelte/transition';
+
+  let { panels }: { panels: PanelDefinition<PanelData>[] } = $props();
+  let data = $state(untrack(() => panels[0]?.data as PanelData));
+
+  const preloadUrls = $derived([
+    ...new Set(panels.map(p => p.data.datawrapperUrl as string).filter(Boolean))
+  ] as string[]);
+</script>
+
+<Scrollyteller
+  {panels}
+  onMarker={newData => {
+    data = newData;
+  }}
+  layout={{
+    align: 'left'
+    // resizeInteractive: true
+    // transparentFloat: true
+  }}
+>
+  {#each preloadUrls as url (url)}
+    <iframe src={url} title="" class:visible={url == data.datawrapperUrl}> </iframe>
+  {/each}
+</Scrollyteller>
+
+<style lang="scss">
+  iframe {
+    border: none;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    transition: opacity 0.25s;
+    opacity: 0;
+    &.visible {
+      opacity: 1;
+    }
+  }
+</style>
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import acto from '@abcnews/alternating-case-to-object';
+import { whenOdysseyLoaded } from '@abcnews/env-utils';
+import { getMountValue, selectMounts } from '@abcnews/mount-utils';
+import type { Mount } from '@abcnews/mount-utils';
+import { loadScrollyteller } from "@abcnews/svelte-scrollyteller";
+import App from './components/App/App.svelte';
+import { mount } from 'svelte';
+
+export type PanelData = {
+  datawrapperUrl: string;
+};
+
+let appMountEl: Mount;
+let appProps;
+
+whenOdysseyLoaded.then(() => {
+
+
+  const scrollyData = loadScrollyteller<PanelData>(
+    "datawrapper", // If set to eg. "one" use #scrollytellerNAMEone in CoreMedia
+    "u-full", // Class to apply to mount point u-full makes it full width in Odyssey
+    "mark", // Name of marker in CoreMedia eg. for "point" use #point default: #mark
+  );
+
+  // Strip out Datawrapper charts
+  let datawrapperUrl = '';
+  const modifiedPanels = scrollyData.panels.map(panel => {
+    const newNodes = panel.nodes.filter(node => {
+      const dwIframe = node.querySelector('iframe[src*=datawrapper]');
+      if (dwIframe) {
+        datawrapperUrl = dwIframe.getAttribute('src') || '';
+        return false;
+      }
+      return true;
+    })
+    return {
+      ...panel,
+      data: { ...panel.data, datawrapperUrl },
+      nodes: newNodes
+    }
+  })
+  mount(App, {
+    target: scrollyData.mountNode,
+    props: { panels: modifiedPanels },
+  })
+
+});
+
+if (process.env.NODE_ENV === 'development') {
+  console.debug(`[interactive-datawrapper-scrollyteller] public path: ${__webpack_public_path__}`);
+}
